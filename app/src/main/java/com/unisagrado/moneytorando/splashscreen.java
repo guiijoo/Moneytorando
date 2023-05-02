a92e71749fd65512cae75ac1ead587a8d26c3507
Paulo Ricardo Lopes Dionizio <venomcraft115@gmail.com>
2023-05-02 20:58:22 -0300

    rola

---
+++ app/src/main/java/com/unisagrado/moneytorando/splashscreen.java
@@ -0,0 +1,26 @@
+package com.unisagrado.moneytorando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            Intent i = new Intent(splashscreen.this,MainActivity.class);
+            startActivity(i);
+            finish();
+            }
+        },5000);
+    }
+}
