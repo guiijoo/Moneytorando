f262b8b12576e128179ff7dc4cc14d2af269f2aa
guiijoo <gui.marchetto@hotmail.com>
2023-05-09 21:23:03 -0300

    atualização teste

--- app/src/main/java/com/unisagrado/moneytorando/login.java
+++ app/src/main/java/com/unisagrado/moneytorando/login.java
@@ -3,12 +3,15 @@ package com.unisagrado.moneytorando;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 
+    private EditText editUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
