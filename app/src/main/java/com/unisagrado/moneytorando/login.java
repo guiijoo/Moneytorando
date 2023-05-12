50fe1fb917a930a1fd0456eeb275683621648640
Luís Guilherme Josepette Marchetto <91769780+guiijoo@users.noreply.github.com>
2023-05-12 16:18:36 -0300

    Update login.java

--- app/src/main/java/com/unisagrado/moneytorando/login.java
+++ app/src/main/java/com/unisagrado/moneytorando/login.java
@@ -12,6 +12,20 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        editTextUsuario = findViewById(R.id.editTextUsuario);
+        editTextSenha = findViewById(R.id.editTextSenha);
+        buttonAvancar = findViewById(R.id.buttonAvancar);
+
+        buttonAvancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = editTextUsuario.getText().toString();
+                String senha = editTextSenha.getText().toString();
+                Intent intent = new Intent(Creditos.this, MainActivity.class);
+                intent.putExtra("usuario", usuario);
+                intent.putExtra("senha", senha);
+                startActivity(intent);
 
     }
 }
