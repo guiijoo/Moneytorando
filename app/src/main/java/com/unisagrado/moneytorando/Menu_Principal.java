91578c0a27e39c0b57fa736775023b26669a1c9c
Paulo <venomcraft115@gmail.com>
2023-05-12 22:26:05 -0300

    Atualização de algumas imagens + arrumei o login.java porque os caracteres estavam dando erro, ai coloquei de acordo com o chat gpt.

--- app/src/main/java/com/unisagrado/moneytorando/Menu_Principal.java
+++ app/src/main/java/com/unisagrado/moneytorando/Menu_Principal.java
@@ -2,14 +2,42 @@ package com.unisagrado.moneytorando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class Menu_Principal extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        button = findViewById(R.id.menu_configurar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exibirOpcoes();
+            }
+        });
+    }
+    private void exibirOpcoes() {
+        CharSequence[] opcoes = {"Opção 1", "Opção 2", "Opção 3"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Escolha uma opção");
+        builder.setItems(opcoes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Lógica para lidar com a opção selecionada
+                String opcaoSelecionada = opcoes[which].toString();
+                Toast.makeText(Menu_Principal.this, "Opção selecionada: " + opcaoSelecionada, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
     }
 
 
--- app/src/main/java/com/unisagrado/moneytorando/login.java
+++ app/src/main/java/com/unisagrado/moneytorando/login.java
@@ -14,24 +14,27 @@ public class login extends AppCompatActivity {
     private EditText editTextUsuario;
     private EditText editTextSenha;
     private Button buttonAvancar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
-        editTextUsuario = findViewById(R.id.editTextUsuario);
-        editTextSenha = findViewById(R.id.editTextSenha);
-        buttonAvancar = findViewById(R.id.buttonAvancar);
+
+        editTextUsuario = findViewById(R.id.usuario_login_txt);
+        editTextSenha = findViewById(R.id.usuario_senha_txt);
+        buttonAvancar = findViewById(R.id.entrar_btn);
 
         buttonAvancar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String usuario = editTextUsuario.getText().toString();
                 String senha = editTextSenha.getText().toString();
-                Intent intent = new Intent(Creditos.this, MainActivity.class);
+                Intent intent = new Intent(login.this, Creditos.class);
                 intent.putExtra("usuario", usuario);
                 intent.putExtra("senha", senha);
                 startActivity(intent);
 
+            }
+        });
     }
 }
