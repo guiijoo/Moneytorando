27138778c166a796a648906b9bdd9f148e975b83
guiijoo <gui.marchetto@hotmail.com>
2023-05-22 16:30:08 -0300

    testando menu

--- app/src/main/java/com/unisagrado/moneytorando/menu_pagamentos_escolhas.java
+++ app/src/main/java/com/unisagrado/moneytorando/menu_pagamentos_escolhas.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.ClipData;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -25,6 +26,8 @@ public class menu_pagamentos_escolhas extends AppCompatActivity {
     private ImageView moradia_image;
     private ImageView outros_image;
     private Button bt_menu_atalho_pagamento;
+    private Menu menu_inicial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +52,17 @@ public class menu_pagamentos_escolhas extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item)
+        {
+            case R.id.goCreditos:
+                Intent goCreditos = new Intent(menu_pagamentos_escolhas.this, creditos.class);
+                startActivity(goCreditos);
+                break;
+
+            case R.id.goInicial:
+                Intent goInicial = new Intent(menu_pagamentos_escolhas.this, inicial.class);
+                break;
+        }
         int id = item.getItemId();
         if(id == R.id.goReceita)
         {
